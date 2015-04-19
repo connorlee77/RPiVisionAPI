319931173f2a3a631a75e83cc208281b2379f882
connor <connorlee77@gmail.com>
2015-04-19 01:56:51 +0000

    DONE: Integrate raspicam API to take image frames with rasperry pi camera

---
+++ build/CMakeFiles/simpletest_raspicam_cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simpletest_raspicam_cv.dir/simpletest_raspicam_cv.cpp.o"
+  "simpletest_raspicam_cv.pdb"
+  "simpletest_raspicam_cv"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simpletest_raspicam_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
