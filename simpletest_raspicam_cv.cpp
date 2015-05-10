5263b6451eabab7c785efc362d32648cdb56cf70
Connor Lee <connorlee77@gmail.com>
2015-05-10 07:14:13 +0000

    cleaned up: thresholding done

--- simpletest_raspicam_cv.cpp
+++ simpletest_raspicam_cv.cpp
@@ -37,7 +37,7 @@ int main ( int argc,char **argv ) {
 	cv::Mat image = captureImage();
 	cv::Mat hsv_frame;
 	cv::cvtColor(image, hsv_frame, CV_BGR2HSV);
-	cv::Scalar min(0/2, 0, 0);
+	cv::Scalar min(0/2, 90, 80);
 	cv::Scalar max(25/2, 255, 255);
 	cv::Mat threshold_frame;
 	cv::inRange(hsv_frame, min, max, threshold_frame);
